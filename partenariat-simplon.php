c94081ed1add363ed919e4766c9c92c146dfdee0
unknown <didier.boutin1@free.fr>
2018-04-20 14:13:12 +0200

    Mise a jour

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,4 +1,29 @@
 <?php
 /*
+Plugin Name: Partenariat Simplon WP
 
+Plugin URI:  
+
+Description: Un plugin  qui permet d'ajouter du texte 
+ 
+Version: 0.1
+
+Author: DidierBoutin
+
+Author URI: 
+
+License: GPL2
 */
+
+function SimplonShortcode() {
+	return '<p>La publication de cet article est possible grace à mon super partenaire 
+<a href="http//simplon.co">simplon.co</a>
+ - une entreprise de 
+l\'économie sociale et solidaire et un
+réseau de "fabriques" (écoles) qui propose 
+des
+formations
+GRATUITES
+pour devenir développeur web. </p>';
+}
+add_shortcode('Simplon', 'SimplonShortcode');
